ad796732383f216a4b7776ffcd38d8840a9ffdae
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2024-01-01 19:47:33 +0530

    QR generator code just added

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -7,6 +7,7 @@ import { AppDispatch } from "../../store"
 import { fetchOneProduct, productState } from "../../Slices/productSlice"
 import { removerUnderScore } from "../All_products/Single_product"
 import { ModelViewer } from "../ModelViewer/ModelViewer"
+import QRCodeGenerator from "../QrGenerator/QrGenerator"
 
 
 
@@ -73,21 +74,25 @@ const ProductDetail = () => {
         <>
             <div className="flex flex-col justify-center items-center text-center rounded mx-1 my-5 ">
 
+                {/* sm is breakpoint for leptop and mobile ----> */}
 
                 <div className=" border rounded flex justify-between items-center flex-col sm:flex-row  w-10/12 sm:w-4/5">
 
 
 
-                    <ModelViewer item={item} height={"75vh"} />
-
+                    <ModelViewer item={item} height={"70vh"} />
 
 
                     <div className=" sm:w-1/4">
 
+                        {/* item name div */}
+                        <div className="flex justify-center items-center flex-wrap flex-row sm:flex-col my-2">
+                            <p className="font-bold uppercase">Dummy</p>
 
+                            <p className=" sm:my-2 px-1 mx-2 uppercase text-xl font-bold border-t  border-2 rounded-lg border-green-400  shadow-lg  hover:scale-110 transition-all">{removerUnderScore(item.name)}</p>
 
-                        <p className=" capitalize font-bold border-t">{removerUnderScore(item.name)}</p>
-
+                            <p className="font-bold uppercase">Dummy</p>
+                        </div>
                         {/* <Link to={`/product/${item.id}`}> */}
                         {/* <button
                         className="border my-1 px-4 rounded font-bold text-white bg-blue-600 hover:bg-blue-400"
@@ -105,14 +110,22 @@ const ProductDetail = () => {
                 </div>
 
 
-                    {/* Goto home BTn */}
-                        <Link to={"/"}>
-                        <button 
+                {/* Generate qr --> */}
+                <div className="my-4">
+
+                    <QRCodeGenerator />
+
+                </div>
+
+
+                {/* Goto home BTn */}
+                <Link to={"/"}>
+                    <button
                         className="border rounded px-4 text-2xl font-bold bg-yellow-500 text-white my-5"
                     >
                         Goto Home 🏠
                     </button>
-                        </Link>
+                </Link>
 
 
             </div>
---
+++ src/components/QrGenerator/QrGenerator.tsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+import QRCode from 'qrcode.react';
+import { useEffect, useState } from 'react';
+
+const QRCodeGenerator = () => {
+
+
+    const [url , setUrl] = useState("")
+
+
+    useEffect(() => {
+        // console.log("get url form url bar ---->")
+
+        const locationObj = document.location
+
+        // console.log(locationObj?.href)
+
+        setUrl(locationObj?.href)
+
+    } , [])
+
+      return <QRCode value={`${url}`} />;
+
+};
+
+export default QRCodeGenerator;
