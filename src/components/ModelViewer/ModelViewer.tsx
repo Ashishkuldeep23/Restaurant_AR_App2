f0d7c8ad0148b9f94148280656f39956c22e8ac9
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2024-01-16 19:32:06 +0530

    Check model now version change and useEffect removed now

--- src/components/ModelViewer/ModelViewer.tsx
+++ src/components/ModelViewer/ModelViewer.tsx
@@ -4,7 +4,7 @@ import "@google/model-viewer/lib/model-viewer";
 // import { useEffect } from "react";
 import { TypeSingleProduct } from "../../Slices/productSlice"
 import "./style.css"
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 
 // import React from 'react'
@@ -31,6 +31,7 @@ interface ModelViewerJSX {
 }
 
 
+
 declare global {
     namespace JSX {
         interface IntrinsicElements {
@@ -45,30 +46,36 @@ declare global {
 
 
 export const ModelViewer = ({ item }: { item: TypeSingleProduct, height?: string }) => {
-// export const ModelViewer = () => {
+    // export const ModelViewer = () => {
 
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        // console.log("Log just to avoid err ->",item)
+    //     // console.log("Log just to avoid err ->",item)
 
-        // Dynamically load the model-viewer script
-        const modelViewerScript = document.createElement('script');
-        modelViewerScript.type = 'module';
-        modelViewerScript.src = 'https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js';
-        document.head.appendChild(modelViewerScript);
+    //     // Dynamically load the model-viewer script
+    //     const modelViewerScript = document.createElement('script');
+    //     modelViewerScript.type = 'module';
+    //     modelViewerScript.src = 'https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js';
+    //     document.head.appendChild(modelViewerScript);
 
-        // // Dynamically load the focus-visible script
-        const focusVisibleScript = document.createElement('script');
-        focusVisibleScript.src = 'https://unpkg.com/focus-visible/dist/focus-visible.js';
-        document.head.appendChild(focusVisibleScript);
+    //     // // Dynamically load the focus-visible script
+    //     const focusVisibleScript = document.createElement('script');
+    //     focusVisibleScript.src = 'https://unpkg.com/focus-visible/dist/focus-visible.js';
+    //     document.head.appendChild(focusVisibleScript);
 
-        // Clean up the scripts when the component is unmounted
-        return () => {
-            document.head.removeChild(modelViewerScript);
-            document.head.removeChild(focusVisibleScript);
-        };
-    }, []);
+    //     // Clean up the scripts when the component is unmounted
+    //     return () => {
+    //         document.head.removeChild(modelViewerScript);
+    //         document.head.removeChild(focusVisibleScript);
+    //     };
+    // }, []);
+
+
+
+    // const androidSrc = item.model.src
+
+    // const iosSrc = item.model.iosSrc ? item.model.iosSrc : "";
 
 
 
@@ -107,15 +114,25 @@ export const ModelViewer = ({ item }: { item: TypeSingleProduct, height?: string
 
                 // src={`./models/${item.name}.glb`}
                 // // // item.model having URL of model ---->
-                id={`three_D_model`}
+                id={`${!item.isNonVeg ? "three_D_model_G" : "three_D_model_R"}`}
+
+
                 // src={`${item.model.src}`}
                 // ios-src={`${item.model.iosSrc}`}
                 src={`/models/cake.glb`}
                 // ios-src={`models/french.usdz`}
-                // camera-controls={true}
-                ar-modes="scene-viewer webxr quick-look"
+
+                // src={androidSrc}
+                // iosSrc={iosSrc}
+
+
+
+
+
+                ar-modes="webxr scene-viewer quick-look"
+                interaction-prompt-threshold="0"
                 alt="3Dmodel"
-                // environment-image="neutral"
+                environment-image="neutral"
                 exposure="1.0"
                 // interaction-prompt-threshold="0"
                 shadow-intensity="1.1"
@@ -128,7 +145,7 @@ export const ModelViewer = ({ item }: { item: TypeSingleProduct, height?: string
                 seamless-poster
                 ar
                 auto-rotate
-                camera-orbit="0deg 90deg 0deg 1m"
+                camera-orbit="0deg 65deg 0deg 1m"
             >
 
 
@@ -143,7 +160,7 @@ export const ModelViewer = ({ item }: { item: TypeSingleProduct, height?: string
                     <div className="update-bar"></div>
                 </div>
                 <button slot="ar-button" id="ar-button">
-                    View {item.name} in your space
+                    View <span className=" capitalize">{item.name}</span> in your space
                 </button>
                 <div id="ar-prompt">
                     <img src="https://modelviewer.dev/shared-assets/icons/hand.png" />
